a210cc72d3b1f34e2644bad9498868682596db5b
Matias Duarte <duarmati@amazon.com>
2023-01-18 11:47:48 -0300

    Express Server

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, { Express, Request, Response } from 'express';
 
 const app: Express = express();
-const port = process.env.PORT;
+const port = 3000;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
